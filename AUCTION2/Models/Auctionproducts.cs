01eb69b6d316f4758df1ae5b7338d1700f03b246
JOHN <johnmickey281@gmail.com>
2020-07-09 02:22:02 -0500

    Add project files.

---
+++ AUCTION2/Models/Auctionproducts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace AUCTION2.Models
+{
+    public class Auctionproducts
+    { 
+        
+        
+        public string Id { get; set; } 
+        public string Title { get; set; }
+            public string Description { get; set; }
+             public string ImageUrl { get; set; }
+             public DateTime StartTime { get; set; }
+             public DateTime EndTime { get; set; }
+             public decimal StartPrice { get; set; }
+             public decimal? CurrentPrice { get; set; }
+             
+    }
+}
